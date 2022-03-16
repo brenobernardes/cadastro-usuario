4475ba20b6687af466ef1ed4bc4326a3b5834179
Breno Bernardes <breno.pbernardes@gmail.com>
2022-03-15 21:28:28 -0300

    first commit

--- cadastro-usuario/src/index.js
+++ cadastro-usuario/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
 
 ReactDOM.render(
-  <Routes />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
